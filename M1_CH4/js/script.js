e97bb1cd738ead848ab4380297d24b1be91beb06
Alexandr Mun <munakaz@yandex.ru>
2021-12-01 20:36:22 +0600

    M1 CH4

---
+++ M1_CH4/js/script.js
@@ -0,0 +1,33 @@
+// let myName = 'Ivan';
+// console.log(myName);
+// let age = 20;
+// console.log(age);
+
+// let a = 5;
+// let b = 10;
+// console.log(a+b);
+
+// console.log(b + ' number');
+
+// let a = parseInt(prompt('insert number a')) ;
+// let b = parseInt(prompt('insert number b'));
+// let sum = a + b;
+// alert('sum' + sum);
+
+// if(a === 5) {
+//     alert('ok');
+// } else {
+//     alert(b);
+// }
+
+function task1() {
+    let x = parseInt(prompt('insert x'))
+    let y = x**2 + 2 * x - 1;
+    alert(y);
+}
+
+// task1();
+
+
+
+
